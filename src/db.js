ca61be64ed82bf879047e65b7af3fcc6bf7898e4
FABIAN <fabianlovera777@gmail.com>
2026-01-16 10:48:05 -0300

    Actualizar db.js con variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
-
 const mysql = require("mysql2/promise");
 
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "", 
-  database: "restaurante_db",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "restaurante_db",
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
+  connectTimeout: 60000
 });
 
 module.exports = pool;
